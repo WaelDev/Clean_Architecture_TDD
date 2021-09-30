18d6b69797f99cc3a0ddfc198ddd1c27d9a9a6a6
Wael <12910823+WaelDev@users.noreply.github.com>
2021-09-30 22:01:23 +0100

    Entities & Use Cases

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failures extends Equatable {
+  final List<Object> properties;
+
+  const Failures({required this.properties});
+
+  @override
+  List<Object> get props => properties;
+}
