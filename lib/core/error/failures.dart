cfb81833873475fde0200746c2a8a3410b05d155
Wael <12910823+WaelDev@users.noreply.github.com>
2021-10-02 15:38:47 +0100

    Contracts of Data Sources

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -8,3 +8,14 @@ abstract class Failures extends Equatable {
   @override
   List<Object> get props => properties;
 }
+
+// General Failures
+class ServerFailure extends Failures {
+  final List<Object> properties;
+  ServerFailure(this.properties) : super(properties: properties);
+}
+
+class CacheFailure extends Failures {
+  final List<Object> properties;
+  CacheFailure(this.properties) : super(properties: properties);
+}
