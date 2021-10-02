ed6eb9bb662376d826fbaf1bf5759bd19984269b
Wael <12910823+WaelDev@users.noreply.github.com>
2021-10-02 11:27:41 +0100

    Domain Layer Refactoring

--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -5,5 +5,5 @@ import '../entities/number_trivia.dart';
 
 abstract class NumberTriviaRepository {
   Future<Either<Failures, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failures, NumberTrivia>> getRandomNumberTrivia(int number);
+  Future<Either<Failures, NumberTrivia>> getRandomNumberTrivia();
 }
--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,17 +1,27 @@
 import 'package:dartz/dartz.dart';
-import 'package:number_trivia_app/core/error/failures.dart';
-import 'package:number_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:equatable/equatable.dart';
 
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failures, NumberTrivia>> execute({
-    required int number,
-  }) async {
-    return await repository.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failures, NumberTrivia>> call(Params params) async {
+    return await repository.getConcreteNumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({required this.number}) : super();
+
+  @override
+  List<Object?> get props => [number];
 }
