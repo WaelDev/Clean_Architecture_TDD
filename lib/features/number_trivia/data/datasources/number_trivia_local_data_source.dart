200456f5ca59ea6825582e41b236fb1c97ce09a3
Wael <12910823+WaelDev@users.noreply.github.com>
2021-10-03 17:49:32 +0100

    Local Data Source

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:number_trivia_app/core/error/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
@@ -8,4 +13,31 @@ abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
 
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl(this.sharedPreferences);
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    // Future which is immediately completed
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(jsonDecode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<bool> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    return sharedPreferences.setString(
+      CACHED_NUMBER_TRIVIA,
+      json.encode(triviaToCache.toJson()),
+    );
+  }
 }
---
+++ test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia_app/core/error/exceptions.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:number_trivia_app/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../fixtures/fixtures_reader.dart';
+import 'number_trivia_local_data_source_test.mocks.dart';
+
+@GenerateMocks([SharedPreferences])
+late NumberTriviaLocalDataSourceImpl dataSource;
+late MockSharedPreferences mockSharedPreferences;
+main() {
+  setUp(() async {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource = NumberTriviaLocalDataSourceImpl(mockSharedPreferences);
+  });
+
+  group('getLastNumberTrivia', () {
+    final tNumberTriviaModel =
+        NumberTriviaModel.fromJson(json.decode(fixtures('trivia_cached.json')));
+
+    test(
+        'should return NumberTrivia from SharedPreferences when there is one in the cache',
+        () async {
+      // arrange
+      when(mockSharedPreferences.getString(any))
+          .thenReturn(fixtures('trivia_cached.json'));
+      // act
+      final result = await dataSource.getLastNumberTrivia();
+      // asert
+      verify(mockSharedPreferences.getString(CACHED_NUMBER_TRIVIA));
+      expect(result, equals(tNumberTriviaModel));
+    });
+
+    test('should throw a CacheException when there is not a cached value', () {
+      // arrange
+      when(mockSharedPreferences.getString(any)).thenReturn(null);
+      // act
+      // Not calling the method here, just storing it inside a call variable
+      final call = dataSource.getLastNumberTrivia;
+      // assert
+      // Calling the method happens from a higher-order function passed.
+      // This is needed to test if calling a method throws an exception.
+      expect(() => call(), throwsA(TypeMatcher<CacheException>()));
+    });
+  });
+
+  group('cacheNumberTrivia', () {
+    final tNumberTriviaModel =
+        NumberTriviaModel(text: 'test trivia', number: 1);
+
+    test('should call SharedPreferences to cache the data', () async {
+      // arrange
+      when(mockSharedPreferences.setString(
+        CACHED_NUMBER_TRIVIA,
+        json.encode(tNumberTriviaModel.toJson()),
+      )).thenAnswer((realInvocation) => Future.value(true));
+      // act
+      var result = await dataSource.cacheNumberTrivia(tNumberTriviaModel);
+      // assert
+      final expectedJsonString = json.encode(tNumberTriviaModel.toJson());
+      verify(mockSharedPreferences.setString(
+        CACHED_NUMBER_TRIVIA,
+        expectedJsonString,
+      ));
+      expect(result, equals(true));
+    });
+  });
+}
